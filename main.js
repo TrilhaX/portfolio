6c84db136cbda81af1105d6a1e399497dc7ff93a
João Vitor Trilha Richartz <141366739+TrilhaX@users.noreply.github.com>
2024-03-14 14:35:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
 window.onload = function() {
     var screenWidth = window.innerWidth;
     var zoomLevel;
+    var fimap = document.getElementsByClassName("fimap");
 
     if (screenWidth < 600) {
+        document.getElementById("githubimg").style.left = "290%"
+        document.getElementById("githubimg").style.top = "-370%"
+        for (var i = 0; i < fimap.length; i++){
+            fimap[i].style.top = "24%"
+        }
         zoomLevel = 50; // Zoom de 50% para telas menores que 600 pixels de largura
     } else if (screenWidth >= 600 && screenWidth > 800) {
+        document.getElementById("githubimg").style.left = "270%"
+        document.getElementById("githubimg").style.top = "-350%"
+        for (var i = 0; i < fimap.length; i++){
+            fimap[i].style.top = "26%"
+        }
         zoomLevel = 67; // Zoom de 67% para telas entre 600 e 799 pixels de largura
     } else if (screenWidth >= 800 && screenWidth > 1200) {
+        document.getElementById("githubimg").style.left = "250%"
+        document.getElementById("githubimg").style.top = "-330%"
+        for (var i = 0; i < fimap.length; i++){
+            fimap[i].style.top = "28%"
+        }
         zoomLevel = 100; // Zoom de 100% para telas entre 800 e 1199 pixels de largura
     } else {
+        document.getElementById("githubimg").style.left = "230%"
+        document.getElementById("githubimg").style.top = "-310%"
+        for (var i = 0; i < fimap.length; i++){
+            fimap[i].style.top = "30%"
+        }
         zoomLevel = 150; // Zoom de 150% para telas maiores ou iguais a 1200 pixels de largura
     }
 
