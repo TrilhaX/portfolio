bd178f2c34edd75eca03c42b8a764b18ed9e0f4d
João Vitor Trilha Richartz <141366739+TrilhaX@users.noreply.github.com>
2024-03-14 14:18:34 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ window.onload = function() {
     var zoomLevel;
 
     if (screenWidth < 600) {
-        document.getElementById("githubimg").style.left = "250px"
+        document.getElementById("githubimg").style.left = "250%"
         zoomLevel = 50; // Zoom de 50% para telas menores que 600 pixels de largura
     } else if (screenWidth >= 600 && screenWidth > 800) {
         document.getElementById("githubimg").style.left = "252%"
         zoomLevel = 67; // Zoom de 67% para telas entre 600 e 799 pixels de largura
     } else if (screenWidth >= 800 && screenWidth > 1200) {
-        document.getElementById("githubimg").style.left = "250px"
+        document.getElementById("githubimg").style.left = "250%"
         zoomLevel = 100; // Zoom de 100% para telas entre 800 e 1199 pixels de largura
     } else {
-        document.getElementById("githubimg").style.left = "250px"
+        document.getElementById("githubimg").style.left = "250%"
         zoomLevel = 150; // Zoom de 150% para telas maiores ou iguais a 1200 pixels de largura
     }
 
